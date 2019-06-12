b69cda5cc0bcc439870acf36124387e4ec16e451
Sebastian <sebadlf@gmail..com>
2019-06-11 22:12:23 -0300

    Env variables

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var axios = require("axios");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var client_id = process.env.client_id;
+var client_secret = process.env.client_secret;
+
+let data = client_id + ":" + client_secret;
+let buff = new Buffer(data);
+let encoded = buff.toString("base64");
+
+console.log(encoded);
+
+let config = {
+  headers: {
+    Authorization: "Basic " + encoded,
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+};
+
+app.use("/auth", function(req, res) {
+  axios
+    .post(
+      "https://accounts.spotify.com/api/token",
+      "grant_type=client_credentials",
+      config
+    )
+    .then(({ data }) => {
+      res.json(data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.send("error");
+    });
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +68,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
